ddb43b165cae3b6155165cf71417505a3c5dd9b1
Milo123459 <milo5huntington@gmail.com>
2020-06-11 10:33:05 +0100

    New update

---
+++ install.js
@@ -0,0 +1,3 @@
+console.log(`
+Welcome to the world of ez-args! We are glad you are joining us :)
+`.trim());
---
+++ uninstall.js
@@ -0,0 +1,3 @@
+console.log(`
+We are sad to see you leaving us over at ez-args.. Can we have you feedback? Create a new github issue over at https://github.com/Milo123459/ez-args and let us know why you uninstalled!
+`.trim())
